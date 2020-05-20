9cebc6ed936a95107dc7dc76dbd5b24d70452434
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-05-20 17:02:53 +0300

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Office-iMac on 2020-05-20.
+//  Copyright © 2020 Tony Jem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
