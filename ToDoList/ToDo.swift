6c4b04a9dea2269cc7a557097230df44cc1a1a0a
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-05-20 17:22:11 +0300

    Define the Model: "ToDo"

---
+++ ToDoList/ToDo.swift
@@ -0,0 +1,16 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Office-iMac on 2020-05-20.
+//  Copyright © 2020 Tony Jem. All rights reserved.
+//
+
+import Foundation
+
+struct ToDo{
+    var title: String
+    var isComplite: Bool
+    var dueDate: Date
+    var notes: String?
+}
