21d4cc720ea080c5e518e077e7ebdf65de8f43c6
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-05-20 17:54:53 +0300

    Supply Initial Data
    Add empty func "loadToDos";
    Add func "loadSampleToDos" with samaple array of model objects;
    Add conditional logic to fill the table view with Data.

--- ToDoList/ToDo.swift
+++ ToDoList/ToDo.swift
@@ -13,4 +13,17 @@ struct ToDo{
     var isComplite: Bool
     var dueDate: Date
     var notes: String?
+    
+//    Load data from file:
+    static func loadToDos() -> [ToDo]? {
+        return nil
+    }
+    
+//    Load sample data from array below:
+    static func loadSampleToDos() -> [ToDo] {
+        let todo1 = ToDo(title: "ToDo One", isComplite: false, dueDate: Date(), notes: "Notes 1")
+        let todo2 = ToDo(title: "ToDo Two", isComplite: false, dueDate: Date(), notes: "Notes 2")
+        let todo3 = ToDo(title: "ToDo Three", isComplite: false, dueDate: Date(), notes: "Notes 3")
+        return [todo1, todo2, todo3]
+    }
 }
