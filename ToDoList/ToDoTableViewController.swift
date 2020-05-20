43cbe4e7d5ba09750a724017474640a189eef7ae
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-05-20 18:40:32 +0300

    Implement "Delete" items
    Add intelegent "Edit" button to main Table View

--- ToDoList/ToDoTableViewController.swift
+++ ToDoList/ToDoTableViewController.swift
@@ -23,11 +23,8 @@ class ToDoTableViewController: UITableViewController {
         } else {
             todos = ToDo.loadSampleToDos()
         }
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-        
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        // Add "Edit" button in navigation bar for switching On/Off "Edit" mode:
+         self.navigationItem.leftBarButtonItem = self.editButtonItem
     }
     
     // MARK: - Table view data source
@@ -53,25 +50,19 @@ class ToDoTableViewController: UITableViewController {
     }
     
     
-    /*
-     // Override to support conditional editing of the table view.
+     // Enable rows to be editable:
      override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
+        return true    // Return false if you do not want the specified item to be editable.
      }
-     */
     
-    /*
-     // Override to support editing the table view.
-     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-     if editingStyle == .delete {
-     // Delete the row from the data source
-     tableView.deleteRows(at: [indexPath], with: .fade)
-     } else if editingStyle == .insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-     }
-     }
-     */
+    // Enable rows to be deleted when "Edit" mode is On:
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data array and from Table View:
+            todos.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
     
     /*
      // Override to support rearranging the table view.
